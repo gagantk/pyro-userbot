2d936c8c57f86e720a417338fde25d2bdf2970db
Gagan T K <gagantk07@gmail.com>
2020-08-10 18:13:17 +0530

    fixed syntax

--- gaganrobot/plugins/mystuffs/transcode.py
+++ gaganrobot/plugins/mystuffs/transcode.py
@@ -3,6 +3,12 @@ from gaganrobot import gaganrobot, Message
 LOGGER = gaganrobot.getLogger(__name__)
 
 
-@gaganrobot.on_cmd('transcode', about: {'header': 'Transcode media files using ffmpeg', 'description': 'Transcode media files using ffmpeg', 'examples': '{tr}transcode input_file opts output_file'})
+@gaganrobot.on_cmd('transcode', about={'header': 'Transcode media files using ffmpeg', 'description': 'Transcode media files using ffmpeg', 'examples': '{tr}transcode input_file opts output_file'})
 async def transcode(message: Message):
     """ transcode media file """
+    await message.edit('`Processing...`')
+    if message.input_str:
+        text = message.input_str
+        options = text.split('|')
+    else:
+        await message.edit("Please read `.help transcode`", del_in=5)
